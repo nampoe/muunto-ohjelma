4528a502bae62ca8b941083f16f67169ea1060d2
veeti-21 <158137597+veeti-21@users.noreply.github.com>
2025-08-25 12:20:38 +0300

    Update Program.cs

--- muunto-ohjelma/Program.cs
+++ muunto-ohjelma/Program.cs
@@ -5,9 +5,9 @@ class Program
     static void Main()
     {
         // Yksiköt ja niiden muuntosuhteet metreihin
-        string[] units = { "millimetri", "senttimetri", "metri", "kilometri", "tuuma", "jalka", "jaardi", "maili" };
+        string[] units = { "Millimetri", "Senttimetri", "Metri","Kilometri", "Tuuma", "Jalka", "Jaardi", "Maili" };
         double[] toMeters = { 0.001, 0.01, 1,1000,0.0254, 0.3048, 0.9144, 1609.344 };
-        
+
         //kysytään yksitöt ja lukumäärät
         Console.WriteLine("Valitse lähtöyksikkö:");
         for (int i = 0; i < units.Length; i++)
@@ -35,11 +35,8 @@ class Program
         for (int i = 0; i < units.Length; i++)
         {
             double converted = meters / toMeters[i];
-            Console.WriteLine($"{units[i]}: {converted:F6}");
+            Console.WriteLine($"{units[i]}: {converted:0.########}");
         }
     }
 }
-
-
-
 
